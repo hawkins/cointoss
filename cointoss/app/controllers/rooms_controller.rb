ff424acccc81f2edd7afe69ddde45a8c0b7b4fc7
jhilbs3 <jhilbs3@gmail.com>
2020-08-06 18:01:49 -0700

    Can now create rooms and they are displayed on the rooms homepage. Need to create room pages and validate room info

--- cointoss/app/controllers/rooms_controller.rb
+++ cointoss/app/controllers/rooms_controller.rb
@@ -1,7 +1,7 @@
 class RoomsController < ApplicationController
 
     def index
-        #@rooms = Room.all
+        @rooms = Room.all
     end
 
     def new
@@ -25,7 +25,7 @@ class RoomsController < ApplicationController
                                               :max_bet,
                                               :min_bet,
                                               :house_wallet,
-                                              :max_players))
+                                              :max_users))
         end
     end
 
--- cointoss/app/models/room.rb
+++ cointoss/app/models/room.rb
@@ -1,13 +1,13 @@
 class Room < ApplicationRecord
-    validates :house_wallet, numericality: {
-        on: :update,
-        less_than_or_equal_to: User.find(:host_id).account_balance,
-        message: "Initial room account balance must be less than or equal to the users account balance."
-    }
+    #validates_on_update :house_wallet, numericality: {
+    #    if: :deposit_into_room?,
+    #    less_than_or_equal_to: User.find(:host_id).account_balance,
+    #    greater_than_or_equal_to: 0,
+    #    message: "Initial room account balance must be less than or equal to the users account balance."
+    #}
     validates :max_bet, numericality: {
-        on: :update,
         less_than_or_equal_to: :house_wallet,
-        message: "Max bet must be less than or equal to the initial room account balance."
+        message: "must be less than or equal to the initial room account balance."
     }
 
 end
