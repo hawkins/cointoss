7b14eb1fb85ed9ffa27e15f2b6040f3b7eb9975b
jhilbs3 <jhilbs3@gmail.com>
2020-08-07 17:00:30 -0700

    still need to validate user account_balance

--- cointoss/app/controllers/rooms_controller.rb
+++ cointoss/app/controllers/rooms_controller.rb
@@ -5,19 +5,18 @@ class RoomsController < ApplicationController
     end
 
     def new
-        @room = Room.create
+        @room = Room.new
+    end
+
+    def update_user_account_balance(host_id, house_wallet)
+        host = User.find_by(id: host_id)
+        new_account_balance = host.account_balance - house_wallet
+        host.update(account_balance: new_account_balance)
     end
 
     def create
 
-        if current_user.account_balance < params[:room][:house_wallet].to_i
-            @room.error = "get more money"
-            render :new
-        else
-
-            current_user.account_balance = current_user.account_balance - params[:room][:house_wallet].to_i
-
-            @room = Room.create(params.require(:room)
+        @room = Room.create(params.require(:room)
                                       .merge!(creation_time: Time.now(),
                                               host_id: current_user.id,
                                               locked: false)
@@ -25,7 +24,14 @@ class RoomsController < ApplicationController
                                               :max_bet,
                                               :min_bet,
                                               :house_wallet,
-                                              :max_users))
+                                              :max_users,
+                                              :host_id,
+                                              :locked))
+            
+        if @room.valid?
+            update_user_account_balance(@room.host_id, @room.house_wallet)
+        else
+            render :new
         end
     end
 
--- cointoss/app/models/room.rb
+++ cointoss/app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
     #validates_on_update :house_wallet, numericality: {
-    #    if: :deposit_into_room?,
+    #    on: :create,
     #    less_than_or_equal_to: User.find(:host_id).account_balance,
     #    greater_than_or_equal_to: 0,
     #    message: "Initial room account balance must be less than or equal to the users account balance."
