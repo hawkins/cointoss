674268e7872105abae63d53ff2b4386a9a9b86e4
Josh Hawkins <hawkins@users.noreply.github.com>
2020-08-08 09:05:43 -0400

    Add bootstrap

--- cointoss/config/boot.rb
+++ cointoss/config/boot.rb
@@ -2,3 +2,8 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+
+# See https://stackoverflow.com/questions/51379326/current-execjs-runtime-doest-support-es6
+# make the ExecJs use NodeJs
+ENV['EXECJS_RUNTIME'] = 'Node'
