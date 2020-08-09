c42b5df6bef5a57dcfc59fb17f97654448ee9b62
Josh Hawkins <hawkins@users.noreply.github.com>
2020-08-08 20:53:11 -0400

    Show errors on room creation fail

--- cointoss/app/controllers/rooms_controller.rb
+++ cointoss/app/controllers/rooms_controller.rb
@@ -1,102 +1,118 @@
 class RoomsController < ApplicationController
-    around_action :set_current_user
+  include RoomsHelper
+  around_action :set_current_user
 
-    def index
-        @rooms = Room.all
+  def index
+    @rooms = Room.all
+  end
+
+  def remove_user
+    @user = User.find_by(id: params[:user_id])
+    @room = Room.find_by(id: params[:room_id])
+    if @user.id != @room.host_id
+      @room.users.delete(@user)
+      @user.room_id = nil
+      redirect_to rooms_path
+    else
+      redirect_back(fallback_location: rooms_path)
+    end
+  end
+
+  def destroy
+    @room = Room.find_by(id: params[:id])
+    @room.users.each do |u|
+      u.room_id = nil
+    end
+    @room.users.delete_all
+    @room.actions.destroy_all
+    @room.bets.delete_all
+    @user = User.find_by(id: @room.host_id)
+    @user.update(room_id: nil)
+    if (@room.house_wallet > 0)
+      @user.update(account_balance: @user.account_balance + @room.house_wallet)
+    end
+    @room.destroy
+    redirect_back(fallback_location: rooms_path)
+  end
+
+  def add_user
+    @user = User.find_by(id: params[:user_id])
+    @room = Room.find_by(id: params[:room_id])
+    @room.users << @user
+    @user.room = @room
+    redirect_to @room
+  end
+
+  def show
+    begin
+      @room = Room.find(params[:id].to_i)
+    rescue ActiveRecord::RecordNotFound
+      redirect_to rooms_path
+    else
+      if !@room.users.exists?(current_user.id)
+        redirect_to rooms_path
+      end
+    end
+  end
+
+  def new
+    @room = Room.new if @room.nil?
+  end
+
+  def update_user_account_balance(host_id, house_wallet)
+    host = User.find_by(id: host_id)
+    new_account_balance = host.account_balance - house_wallet
+    host.update(account_balance: new_account_balance)
+  end
+
+  def create
+    @room = Room.new(params.require(:room)
+                         .merge!(creation_time: Time.now(),
+                                 host_id: current_user.id,
+                                 locked: false,
+                                 room_state: STANDBY_STAGE)
+                         .permit(:name,
+                                 :max_bet,
+                                 :min_bet,
+                                 :house_wallet,
+                                 :max_users,
+                                 :host_id,
+                                 :locked,
+                                 :room_state)) if @room.nil?
+
+    if current_user.room_id.present?
+      @room.errors.add(:base, "Cannot create more than one room.")
+
+      render :new
+      return
     end
 
-    def remove_user
-        @user = User.find_by(id: params[:user_id])
-        @room = Room.find_by(id: params[:room_id])
-        if @user.id != @room.host_id
-            @room.users.delete(@user)
-            @user.room_id = nil
-            redirect_to rooms_path
-        else
-            redirect_back(fallback_location: rooms_path)
-        end
+    if @room.valid?
+      @room.save
+      update_user_account_balance(@room.host_id, @room.house_wallet)
+      User.find_by(id: @room.host_id).update(room_id: @room.id)
+      redirect_to @room
+    else
+      render :new
     end
+  end
 
-    def destroy
-        @room = Room.find_by(id: params[:id])
-        @room.users.each do |u|
-            u.room_id = nil
-        end
-        @room.users.delete_all
-        @room.actions.destroy_all
-        @room.bets.delete_all
-        @user = User.find_by(id: @room.host_id)
-        @user.update(room_id: nil)
-        if(@room.house_wallet > 0)
-            @user.update(account_balance: @user.account_balance + @room.house_wallet)
-        end
-        @room.destroy
-        redirect_back(fallback_location: rooms_path)
-    end
+  def set_current_user
+    RoomsHelper.user = current_user
+    yield
+  end
 
-    def add_user
-        @user = User.find_by(id: params[:user_id])
-        @room = Room.find_by(id: params[:room_id])
-        @room.users << @user
-        @user.room = @room
-        redirect_to @room
-    end
+  # Progress room to the next stage, updating @room.room_state
+  #
+  # @room.state has these values:
+  #   0: Standby - Admin will update actions and odds now
+  #   1: Betting - Odds are locked and players can place bets now
+  #   2: Playing - Game is in progress, bets are locked, admin can reward bets now
+  def next_stage
+    @room = Room.find(params[:id])
+    @room.room_state += 1
+    @room.room_state = RoomsHelper.STANDBY_STAGE if @room.room_state > RoomsHelper.PLAYING_STAGE
 
-    def show
-        begin
-            @room = Room.find(params[:id].to_i)
-        rescue ActiveRecord::RecordNotFound
-            redirect_to rooms_path
-        else
-            if !@room.users.exists?(current_user.id)
-                redirect_to rooms_path
-            end
-        end
-    end
-
-    def new
-        @room = Room.new
-    end
-
-    def update_user_account_balance(host_id, house_wallet)
-        host = User.find_by(id: host_id)
-        new_account_balance = host.account_balance - house_wallet
-        host.update(account_balance: new_account_balance)
-    end
-
-    def create
-
-        @room = Room.new
-        if current_user.room_id.present?
-            @room.errors.add(:room, message: "Cannot create more than one room.")
-            @room.save
-            redirect_back(fallback_location: rooms_path)
-            return 
-        end
-
-        @room = Room.create(params.require(:room)
-                                      .merge!(creation_time: Time.now(),
-                                              host_id: current_user.id,
-                                              locked: false)
-                                      .permit(:name,
-                                              :max_bet,
-                                              :min_bet,
-                                              :house_wallet,
-                                              :max_users,
-                                              :host_id,
-                                              :locked))
-            
-        if @room.valid?
-            update_user_account_balance(@room.host_id, @room.house_wallet)
-            User.find_by(id: @room.host_id).update(room_id: @room.id)
-            redirect_to rooms_path
-        else
-            render :new
-        end
-    end
-
-    def set_current_user
-        RoomsHelper.user = current_user
-        yield
-    end
+    redirect_back(fallback_location: @room)
+  end
 end
--- cointoss/app/models/bet.rb
+++ cointoss/app/models/bet.rb
@@ -3,20 +3,20 @@ class Bet < ApplicationRecord
       on: :create,
       less_than_or_equal_to: RoomsHelper.user.account_balance,
       greater_than_or_equal_to: 0,
-      message: "You can't afford that bet!"
+      message: "must be greater than 0 and less than your current account balance."
   }
 
   validates :wager_amount, numericality: {
       on: :create,
       less_than_or_equal_to: RoomsHelper.room.max_bet,
       greater_than_or_equal_to: RoomsHelper.room.min_bet,
-      message: "Your bet must be between the min and max bets of the room."
+      message: "must be between the min and max bets of the room."
   }
 
   validates :wager_amount, numericality: {
       on: :create,
       less_than_or_equal_to: RoomsHelper.room.house_wallet,
-      message: "The house can't afford to take that bet. They're not backed by gold, you know. Try a lower bet."
+      message: "must be lower than the house wallet. They just can't afford it, dumbass. They're not backed by gold, you know. Try a lower bet."
   }
 
   belongs_to :room
