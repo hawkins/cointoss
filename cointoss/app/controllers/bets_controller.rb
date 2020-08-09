e3c840f836ed5e171e0decacf1be25aa5af4348c
Josh Hawkins <hawkins@users.noreply.github.com>
2020-08-08 22:17:46 -0400

    Incomplete validation logic

--- cointoss/app/controllers/bets_controller.rb
+++ cointoss/app/controllers/bets_controller.rb
@@ -1,9 +1,26 @@
 class BetsController < ApplicationController
+  include RoomsHelper
   around_action :set_room
 
   def create
     params.each do |key, value|
       @room = Room.find_by(id: params[:room_id])
+
+      if @room.room_state != BETTING_STAGE
+        if @room.room_state == PLAYING_STAGE
+          @room.errors.add(:room, "You cannot place bets while the game is in progress.")
+        end
+        if @room.room_state == STANDBY_STAGE
+          @room.errors.add(:room, "You cannot place bets while the host is updating odds.")
+        end
+
+        @room.save
+
+        puts @room.errors.messages
+
+        redirect_back(fallback_location: @room)
+        return
+      end
 
       if key.start_with? "bet_"
         wager = params[key][:wager_amount].to_i
