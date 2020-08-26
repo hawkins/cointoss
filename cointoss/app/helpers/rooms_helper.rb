a3647aa7d11b8ffcf15cc05065b42bf28664d7e0
Josh Hawkins <hawkins@users.noreply.github.com>
2020-08-26 19:55:08 -0400

    Default values for room helper

--- cointoss/app/helpers/rooms_helper.rb
+++ cointoss/app/helpers/rooms_helper.rb
@@ -2,6 +2,14 @@ module RoomsHelper
   thread_mattr_accessor :user
   thread_mattr_accessor :room
 
+  def user
+    @user || User.create
+  end
+
+  def room
+    @room || Room.create
+  end
+
   STANDBY_STAGE = 0.freeze
   BETTING_STAGE = 1.freeze
   PLAYING_STAGE = 2.freeze
